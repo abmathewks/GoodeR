e152d5f5e43a87062f975f5715db632ac6a8d1a6
Abraham Mathew <mathewanalytics@gmail.com>
2022-01-03 10:04:14 -0700

    add helper functions initial

---
+++ R/HelperFunctions.R
@@ -0,0 +1,175 @@
+### helper_functions.R
+
+
+###################################################################################################
+
+
+ 
+#' @title CreateProjectFiles
+#'
+#' @description Check a directory and create a project and folders if they don't exist
+#'
+#' @author Abraham Mathew
+#' @family Project Preparation
+#'
+#' @param PROJECT_PATH The main project directory
+#' @param FOLDERS_TO_CREATE The folders that will be created in the project path
+#' @param OPEN_NEW_SESSION If TRUE, a new session will be generated
+#' @param DEBUG If TRUE, the function will run in debug mode
+#'
+#' @return Creates a project and folders in a directory if a RStudio project is not present
+#'
+#' @examples
+#' \donttest{
+#'
+#'      CreateProjectFiles("/path/to/directory")
+#'
+#' }
+#' @export
+#' 
+CreateProjectFiles <- function(PROJECT_PATH = rstudioapi::getActiveProject(),
+                               FOLDERS_TO_CREATE = c("data", "docs", "figs", "logs",
+                                                     "output", "queries", "R", "tests"),
+                               OPEN_NEW_SESSION = TRUE,
+                               DEBUG = TRUE){
+
+    if(DEBUG) message("CreateProjectFiles: Function initialized  \n")
+  
+    if(!dir.exists(PROJECT_PATH)){
+        stop("CreateProjectFiles: Project path does not exist  \n")
+    }
+  
+    if(!getwd() == PROJECT_PATH){
+        stop("CreateProjectFiles: Project path argument is different from the current working directory  \n")
+    }
+  
+    FUNCTION_OUTPUT <- list()
+  
+    FUNCTION_OUTPUT[["CURRENT_DIRECTORY"]] <- getwd()
+    FUNCTION_OUTPUT[["PROJECT_PATH"]] <- PROJECT_PATH
+    FUNCTION_OUTPUT[["DIRECTORY_EXISTS"]] <- dir.exists(PROJECT_PATH)
+    FUNCTION_OUTPUT[["RPROJ_FILE_PRESENT"]] <- any(grepl("Rproj", list.files()))
+    FUNCTION_OUTPUT[["FOLDERS_PRESENT_ORIG"]] <- all(FOLDERS_TO_CREATE %in% list.files(PROJECT_PATH))
+  
+    if(!any(grepl("Rproj", list.files()))){
+  
+        if(DEBUG) message("CreateProjectFiles: Project is not present  \n")
+  
+        usethis::create_project(PROJECT_PATH, open = OPEN_NEW_SESSION)
+  
+    }
+  
+    if(!all(FOLDERS_TO_CREATE %in% list.files(PROJECT_PATH))){
+  
+        if(DEBUG) message("CreateProjectFiles: Creating files  \n")
+  
+        for(each_folder in FOLDERS_TO_CREATE){
+  
+          dir.create(file.path(PROJECT_PATH, each_folder), showWarnings = FALSE)
+  
+          if(each_folder == "data"){
+  
+             dir.create(file.path(PROJECT_PATH, each_folder, "raw"), showWarnings = FALSE)
+             dir.create(file.path(PROJECT_PATH, each_folder, "processed"), showWarnings = FALSE)
+  
+           }
+       }
+    } else {
+  
+        if(DEBUG) message("CreateProjectFiles: FOLDERS_TO_CREATE already present in the PROJECT_PATH  \n")
+      
+    }
+  
+    FUNCTION_OUTPUT[["FOLDERS_PRESENT_NEW"]] <- all(FOLDERS_TO_CREATE %in% list.files(PROJECT_PATH))
+    FUNCTION_OUTPUT[["OPEN_NEW_SESSION"]] <- OPEN_NEW_SESSION
+  
+    if(all(FOLDERS_TO_CREATE %in% list.files(PROJECT_PATH))){
+        if(DEBUG) message("CreateProjectFiles: Function run completed  \n")
+  
+        return(FUNCTION_OUTPUT)
+    } else {
+        stop("CreateProjectFiles: Folders not created  \n")
+    }
+}
+
+
+
+###################################################################################################
+
+ 
+
+#' @title PackageCheck
+#'
+#' @description Check if a vector of packages is installed. If they are not, install and load them
+#'
+#' @author Abraham Mathew
+#' @family Project Preparation
+#'
+#' @param USE_THESE_PACKAGES A vector of strings which describe the packages that will be used during the project
+#' @param DEBUG If TRUE, the function will run in debug mode
+#'
+#' @return Installs any missing packages and loads of them into the current workspace
+#'
+#' @examples
+#' \donttest{
+#'
+#'      PackageCheck(c("ggplot2","data.table","lubridate","rms"))
+#'
+#' }
+#' @export
+#' 
+PackageCheck <- function(USE_THESE_PACKAGES,
+                         DEBUG = TRUE){
+ 
+    if(DEBUG) message("PackageCheck: Function initialized  \n")
+
+    if(any(USE_THESE_PACKAGES == "")){
+         stop("PackageCheck: An empty string element was passed in USE_THESE_PACKAGES  \n")
+    } 
+    
+    if(DEBUG) message("PackageCheck: Check for uninstalled packages  \n")
+
+    FUNCTION_OUTPUT <- list()
+
+    new_packages <- USE_THESE_PACKAGES[!(USE_THESE_PACKAGES %in% installed.packages()[,"Package"])]
+
+    if(length(new_packages)) {
+          
+        install.packages(new_packages)
+
+        if(DEBUG) message("PackageCheck: Importing all packages  \n")
+
+        sapply(USE_THESE_PACKAGES[tolower(USE_THESE_PACKAGES) %in% tolower(.packages())], require, character.only = TRUE)
+
+    } else {
+      
+        if(DEBUG) message("PackageCheck: Importing all packages  \n")
+
+        sapply(USE_THESE_PACKAGES[tolower(USE_THESE_PACKAGES) %in% tolower(.packages())], require, character.only = TRUE)
+
+    }
+
+    if(DEBUG) message("PackageCheck: Collecting final output  \n")
+
+    results <- data.table(package_name = names(sapply(USE_THESE_PACKAGES, require, character.only = TRUE)),
+                          loaded = sapply(USE_THESE_PACKAGES, require, character.only = TRUE))
+
+    FUNCTION_OUTPUT[["USE_THESE_PACKAGES"]] <- USE_THESE_PACKAGES
+    FUNCTION_OUTPUT[["NEW_PACKAGES_TO_INSTALL"]] <- new_packages
+    FUNCTION_OUTPUT[["ALREADY_LOADED"]] <- USE_THESE_PACKAGES[tolower(USE_THESE_PACKAGES) %in% tolower((.packages()))]
+    FUNCTION_OUTPUT[["FINAL_PACKAGE_STATUS"]] <- results
+
+    if(!is.null(results) || !nrow(results) == 0){
+        if(DEBUG) message("PackageCheck: Package installation completed  \n")
+
+        return(FUNCTION_OUTPUT)
+    } else {
+        stop("PackageCheck: Function returned an empty data set  \n")
+    }
+}
+
+
+
+
+###################################################################################################
+
